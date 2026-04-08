0f1bfeb445f89451309fbf917cee360671af38ab
tz-studio-office <tz.studio.office@gmail.com>
2026-04-08 13:19:23 +0900

    Delete mercari-arbitrage-monitor-workers-root-nolock directory

--- mercari-arbitrage-monitor-workers-root-nolock/src/lib/hash.ts
+++
@@ -1,6 +0,0 @@
-export async function sha256Hex(input: string): Promise<string> {
-  const data = new TextEncoder().encode(input);
-  const digest = await crypto.subtle.digest('SHA-256', data);
-  const bytes = Array.from(new Uint8Array(digest));
-  return bytes.map((b) => b.toString(16).padStart(2, '0')).join('');
-}
